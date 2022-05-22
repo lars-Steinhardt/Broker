29272c00c5ce0a4bd20dbed4777058fee6050b0a
Lars Steinhardt <steinhardtl@bzz.ch>
2022-05-22 21:34:44 +0200

    Milestone 1 Commit 1

---
+++ src/main/java/ch/bzz/broker/model/Broker.java
@@ -0,0 +1,25 @@
+package ch.bzz.broker.model;
+
+/**
+ * a book publisher
+ */
+public class Broker {
+    private String brokerID;
+    private String brokerName;
+
+    public String getBrokerID() {
+        return brokerID;
+    }
+
+    public void setBrokerID(String brokerID) {
+        this.brokerID = brokerID;
+    }
+
+    public String getBrokerName() {
+        return brokerName;
+    }
+
+    public void setBrokerName(String brokerName) {
+        this.brokerName = brokerName;
+    }
+}
